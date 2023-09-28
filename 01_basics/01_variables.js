15415be4652cc7fde439c86ec99fb61ef4b819cb
MathewJoseph2005 <135580137+MathewJoseph2005@users.noreply.github.com>
2023-09-28 23:14:24 +0000

    datatypes and variables

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -3,6 +3,16 @@ let accountEmail = "indiasaudi1122@gmail.com"
 var accountPassword = "1234"
 accountCity = "kochi"
 
-accountId = 2
+// accountId = 2 // sir ithu work akulla
 
+accountEmail = "check@email.com"
+
+accountPassword = "53425325"
+
+accountCity = "kottayam"
+/*
+prefer not to use var, because of issue in block scope and functional scope
+*/
 console.log(accountId);
+
+console.table([accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+/* when we use "use strict" JS will be executed as a new Js
+*/
+"use strict";
+// alert(3+3) // when we use node js we cond use alert like thisfvcf
+
+// data types
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol (react stuff) => unique
+// object
+console.log(typeof "hello");
