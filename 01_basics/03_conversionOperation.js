b11c43409ef169ecbf1b897721df3ef5512c588d
MathewJoseph2005 <135580137+MathewJoseph2005@users.noreply.github.com>
2023-10-05 01:30:09 +0000

    comit of convertions and comparison

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,40 @@
+let score = 33;
+// console.log(typeof score);
+
+// convert to string 
+
+
+// const numberToString = String(score)
+// console.table([numberToString,typeof numberToString]);
+
+
+// operation
+
+let value = 3;
+let negvalue = -value;
+// console.log(negvalue);
+
+// basic operation
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// contactination
+
+let str_1 = "I love";
+let str_2 = " someone";
+let str_3 = str_1 + str_2;
+// console.log(str_3);
+
+// confusing concepts
+
+
+// console.log('1' + 2 + 2);=>122
+// console.log(1 + 2 + '2');=>32
+// console.log(1+3+5+6+'9'+'8');=>1598
+
+ 
---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,16 @@
+// console.log(2 > 1);=>true
+// console.log(2 >= 1);=>true
+// console.log(2 < 1);=>flase
+// console.log(2 == 1);=>false
+// console.log(2 != 1);=>true
+
+// <,> and == works diffrently
+// <,> would convert null or undefined to 0
+
+// console.log(undefined == 0);//=> false
+// console.log(undefined > 0);//=> false
+// console.log(undefined < 0);//=. false
+
+// console.log(null == 0);//=>false
+// console.log(null > 0);//=> false
+// console.log(null >= 0);//=>true(since,">" converts null to number)
